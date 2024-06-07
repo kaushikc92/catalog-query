988904e1c9057780048f5a024a6a619ae2c638b6
Kaushik Chandrasekhar <kaushik.uwmadison@gmail.com>
2024-06-07 08:42:49 -0700

    Update

--- build_dataset.py
+++ build_dataset.py
@@ -4,18 +4,19 @@ from tfidf import build_index, search_index, delete_index
 train_path = 'kaushikchan/catalog-sql-train'
 test_path = 'kaushikchan/catalog-sql-test'
 
-schema_path = '/Users/kaushik/phd/cq-old/schema/schema.json'
+schema_path = 'schema/sql/schema.json'
 schema = json.load(open(schema_path, 'r'))
 prompts = []
 instruction = 'Translate english queries to SQL.'
-query_path = '/Users/kaushik/phd/cq-old/queries/queries.json'
+query_path = 'queries/queries.json'
 query_dataset = load_dataset("json", data_files=query_path, split='train')
-query_dataset = query_dataset.train_test_split(train_size=309, test_size=350)
+query_dataset = query_dataset.train_test_split(train_size=500, test_size=700)
 
 for query in query_dataset['train']:
     context = 'Schema: '
     tables = query['tables']
-    while len(tables) < 5:
+    n_tables = random.randint(5, 10)
+    while len(tables) < n_tables:
         table = random.choice(list(schema.keys()))
         if table not in tables:
             tables.append(table)
@@ -35,7 +36,10 @@ train_set.push_to_hub(train_path)
 prompts = []
 for query in query_dataset['test']:
     context = 'Schema: '
-    tables = search_index(query['naturalLanguageQuery'])
+    s1 = set(query['tables_from_tfidf'])
+    s2 = set(query['tables_from_embeddings'])
+    tables = list(s1 | s2)
+    random.shuffle(tables)
     for table in tables:
         context += table + ': '
         for column in schema[table]:
