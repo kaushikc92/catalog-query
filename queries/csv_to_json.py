777d0e4385b7aaccc816bc49f73f3d09a3b92a20
BrianZCS <zzheng94@wisc.edu>
2024-08-14 03:58:09 -0500

    add json file following the format

--- queries/csv_to_json.py
+++ queries/csv_to_json.py
@@ -1,16 +1,22 @@
-import csv
+import pandas as pd
 import json
 
 # Read the CSV file
 csv_file = 'queries.csv'
-json_file = 'queries.json'
+json_file = "queries.json"
+df = pd.read_csv(csv_file)
 
-data = []
-with open(csv_file) as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        data.append(row)
+# Rename columns
+df.rename(columns={
+    'example_id': 'eid',
+    'nl_query': 'naturalLanguageQuery',
+    'gold_query': 'goldSqlQuery',
+    # Add more columns as needed
+}, inplace=True)
 
-# Write to JSON file
+# Convert to JSON
+json_data = df.to_json(orient='records')
+
+# Save to a file
 with open(json_file, 'w') as f:
-    json.dump(data, f, indent=4)
+    json.dump(json.loads(json_data), f, indent = 4)
